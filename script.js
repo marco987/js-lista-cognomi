bc2cb6e20ff5c8a03f3455d32ae839c3fc734393
marco987 <pacemarco87@gmail.com>
2019-07-03 14:57:46 +0200

    Push in array

--- script.js
+++ script.js
@@ -4,3 +4,21 @@
 // e stampa la lista ordinata alfabeticamente.
 // Scrivi a che posizione della lista
 // il nuovo utente si trova.
+
+
+// Dichiarazione variabili
+var contenitore_cognomi, cognome_utente;
+
+// Array originale
+contenitore_cognomi = ["Rossi", "Bianchi", "Azzurri", "Zorro"];
+
+// Richiesta cognome all'utente
+cognome_utente = prompt("Qual è il tuo cognome?");
+
+// Push su array
+contenitore_cognomi.push(cognome_utente);
+
+// Stampa lista cognomi in ordine alfabetico
+
+
+// Stampa posizione cognome utente
