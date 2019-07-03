c73b1c5aef26de458236a37511beabc5d9a028d6
marco987 <pacemarco87@gmail.com>
2019-07-03 14:46:07 +0200

    Creazione file

---
+++ script.js
@@ -0,0 +1,6 @@
+// TRACCIA
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi
+// e stampa la lista ordinata alfabeticamente.
+// Scrivi a che posizione della lista
+// il nuovo utente si trova.
